9b7b16ee0877dcad6298874c771d52e6283ac59c
kingdo <1440852110@qq.com>
2023-12-19 17:53:41 +0800

    Rename DataFunction to StateFunction.

---
+++ build-tag-push.sh
@@ -0,0 +1,2 @@
+./gradlew :core:python310Action:distDocker -PdockerImagePrefix=kingdo -PdockerRegistry=docker.io
+#./gradlew :core:python27Action:distDocker -PdockerImagePrefix=kingdo -PdockerRegistry=docker.io
