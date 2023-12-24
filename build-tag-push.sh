4c45441535d62127d89106442cb42779357516b9
kingdo <1440852110@qq.com>
2023-12-24 12:32:05 +0800

    A better script for updating python packages and docker images

--- build-tag-push.sh
+++ build-tag-push.sh
@@ -1,2 +1,9 @@
+#!/bin/bash
+
+# Run `git clone https://github.com/Kingdo777/ChestBox.git` for getting the project.
+ProjectRoot=$(dirname "$(dirname "$(dirname "$(realpath "$0")")")")
+
+sh -c "$ProjectRoot/tools/update-packages.sh"
+
 ./gradlew :core:python310Action:distDocker -PdockerImagePrefix=kingdo -PdockerRegistry=docker.io
 #./gradlew :core:python27Action:distDocker -PdockerImagePrefix=kingdo -PdockerRegistry=docker.io
